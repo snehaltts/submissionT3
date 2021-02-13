d2f347066172d4c83964db2fb82ea1c9c71dcb76
yshchhipa <78848988+yshchhipa@users.noreply.github.com>
2021-02-13 14:24:44 +0530

    binaryToOctal

---
+++ Implementation/src/menu.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void printMenu()
+{
+    cout << "Number Conversion System" << endl;
+    cout << "Enter 1 for binary to decimal Conversion" << endl;
+    cout << "Enter 2 for decimal to binary Conversion" << endl;
+    cout << "Enter 3 for binary to octal Conversion" << endl;
+    cout << "Enter 4 for octal to binary Conversion" << endl;
+    cout << "Enter 5 for hexadecimal to decimal conversion" << endl;
+}
